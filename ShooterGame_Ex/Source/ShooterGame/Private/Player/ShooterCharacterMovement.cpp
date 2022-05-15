c52f02266e0b0f2816290f88fba3d2fbedb0632e
Andrea Agostini <andrea.agostini@witapp.it>
2022-05-15 16:02:44 +0200

    Added network functions

--- ShooterGame_Ex/Source/ShooterGame/Private/Player/ShooterCharacterMovement.cpp
+++ ShooterGame_Ex/Source/ShooterGame/Private/Player/ShooterCharacterMovement.cpp
@@ -44,4 +44,32 @@ void UShooterCharacterMovement::PhysCustom(float deltaTime, int32 Iterations) {
 	}
 }
 
+FNetworkPredictionData_Client_Shooter::FNetworkPredictionData_Client_Shooter(const UCharacterMovementComponent& ClientMovement)
+	: Super(ClientMovement)
+{
+
+}
+
+FSavedMovePtr FNetworkPredictionData_Client_Shooter::AllocateNewMove() {
+	return FSavedMovePtr(new FSavedMove_ShooterCharacter());
+}
+
+void FSavedMove_ShooterCharacter::Clear() {
+	Super::Clear();
+	savedUseJetpack = false;
+}
+
+void FSavedMove_ShooterCharacter::SetMoveFor(ACharacter* Character, float InDeltaTime, FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData) {
+	Super::SetMoveFor(Character, InDeltaTime, NewAccel, ClientData);
+	const AShooterCharacter* ShooterCharacterOwner = Cast<AShooterCharacter>(Character);
+	if (ShooterCharacterOwner)
+		savedUseJetpack = ShooterCharacterOwner->Jetpack1P->bUseJetpack;
+}
+
+void FSavedMove_ShooterCharacter::PrepMoveFor(ACharacter* Character) {
+	Super::PrepMoveFor(Character);
+
+	const AShooterCharacter* ShooterCharacterOwner = Cast<AShooterCharacter>(Character);
+	if (ShooterCharacterOwner)
+		ShooterCharacterOwner->Jetpack1P->ExecJetpack(savedUseJetpack);
 }
--- ShooterGame_Ex/Source/ShooterGame/Public/Player/ShooterCharacterMovement.h
+++ ShooterGame_Ex/Source/ShooterGame/Public/Player/ShooterCharacterMovement.h
@@ -23,5 +23,33 @@ class UShooterCharacterMovement : public UCharacterMovementComponent
 	virtual void PhysCustom(float deltaTime, int32 Iterations) override;
 };
 
+
+/** represents a saved move on the client that has been sent to the server and might need
+to be played back. */
+class FSavedMove_ShooterCharacter : public FSavedMove_Character
+{
+
+	friend class UShooterCharacterMovement;
+
+public:
+	typedef FSavedMove_Character Super;
+	/** Clear saved move properties, so it can be re-used. */
+	virtual void Clear() override;
+	/** Called to set up this saved move (when initially created) to make a predictive correction. */
+	virtual void SetMoveFor(ACharacter* Character, float InDeltaTime, FVector const& NewAccel, class FNetworkPredictionData_Client_Character& ClientData) override;
+
+	/** Called before ClientUpdatePosition uses this SavedMove to make a predictive correction */
+	virtual void PrepMoveFor(ACharacter* Character) override;
+
+	bool savedUseJetpack = false;
+};
+
+/** Network data representation on the client. */
+class FNetworkPredictionData_Client_Shooter : public FNetworkPredictionData_Client_Character
+{
+public:
+	FNetworkPredictionData_Client_Shooter(const UCharacterMovementComponent& ClientMovement);
+	typedef FNetworkPredictionData_Client_Character Super;
+	virtual FSavedMovePtr AllocateNewMove() override;
 };
 
